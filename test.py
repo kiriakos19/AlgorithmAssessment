9991628d584eb124beae6e57d63f67423341ada4
cool <cool@ubuntu2004.myguest.virtualbox.org>
2023-06-06 21:27:17 +0300

    Minnor changes

--- test.py
+++ test.py
@@ -1,17 +1,23 @@
-from tutorialai import Model
+from mainLibrary import Model, EvaluateModel
 
 
-print("ola kala")
+print("Test begin")
 
 enviroment_name = 'Breakout-v0'
 policy = 'CnnPolicy'
 algorithm = 'A2C'
 n_envs=4
 n_stack=4
-total_timesteps=10
+total_timesteps=22
 seed=0
 verbose=1
+fullPath = 'models/A2C_CnnPolicy_300000.zip'
 
-x = Model(enviroment_name, algorithm, policy, n_envs, n_stack, seed, verbose,total_timesteps)
-x.analyzeEnv()
-x.trainModel()
+# evaluateModel= EvaluateModel(fullPath, enviroment_name)
+# evaluateModel.evaluateModel()
+
+
+newModel = Model(enviroment_name, algorithm, policy, n_envs, n_stack, seed, verbose,total_timesteps)
+newModel.analyzeEnv()
+newModel.trainModel()
+newModel.evaluateModel()
