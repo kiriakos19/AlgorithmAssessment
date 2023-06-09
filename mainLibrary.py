092ba70d211b4cea37a436bcb8fab56503c138e5
cool <cool@ubuntu2004.myguest.virtualbox.org>
2023-06-09 13:33:45 +0300

    Add gamma and learning_rate properties

--- mainLibrary.py
+++ mainLibrary.py
@@ -16,7 +16,7 @@ import os
 #!python3 -m atari_py.import_roms /home/mypc/
 
 class Model:
-    def __init__(self,enviroment_name, algorithm, policy, n_envs, n_stack, seed, verbose,total_timesteps):
+    def __init__(self,enviroment_name, algorithm, policy, n_envs, n_stack, seed, verbose,total_timesteps,environment_type,learning_rate, gamma, is_image_based):
         self.enviroment_name = enviroment_name
         self.algorithm= algorithm
         self.policy = policy
@@ -24,37 +24,65 @@ class Model:
         self.n_stack = n_stack
         self.seed = seed
         self.verbose = verbose
+        self.enviroment_type = environment_type
+        self.learning_rate = learning_rate
+        self.gamma = gamma
+        self.is_image_based = is_image_based
         self.total_timesteps = total_timesteps
-        self.log_path = f"logs/{self.algorithm}_{self.policy}_{self.total_timesteps}"
-        self.model_save_path = f"models/{self.algorithm}_{self.policy}_{self.total_timesteps}"
+        self.log_path = f"logs/{self.algorithm}_{self.enviroment_name}_{self.policy}_timesteps:{self.total_timesteps}_gamma:{self.gamma}_learningRate{self.learning_rate}"
+        self.model_save_path = f"models/{self.algorithm}_{self.policy}_timestepes:{self.total_timesteps}_gamma:{self.gamma}_learningRate{self.learning_rate}"
+
 
     def analyzeEnv(self):
-        env = make_atari_env(self.enviroment_name)        
-        env.reset()
+        env = gym.make(self.enviroment_name)  
 
         print("Environment action space: ",env.action_space)
         print("Sample action: ", env.action_space.sample())
         print("Observation space shape: ", env.observation_space.shape)
         print("Sample observation: ", env.observation_space.sample())
+        print("Reward Range: ",env.reward_range)
+        print("Maximum Episode Steps: ", env.spec.max_episode_steps)
 
-
+        observation_space = env.observation_space   
+        is_image_based = isinstance(observation_space, gym.spaces.Box) and len(observation_space.shape) == 3
+        if is_image_based:
+            print("The environment provides image-based observations.")
+        else:
+            print("The environment does not provides image-based observations.")
+        env.reset()
+        return is_image_based
 
 
     def trainModel(self):
-        env = gym.make(self.enviroment_name)
-
-        env.reset()    
-        env = make_atari_env(self.enviroment_name, n_envs=self.n_envs, seed= self.seed)
-        env = VecFrameStack(env, n_stack=self.n_stack)
-    
+#initialize default values for gamma
+        if (self.gamma is None):
+            self.gamma = 0.99
+        
+        if (self.enviroment_type == "Atari"):
+            env = make_atari_env(self.enviroment_name, n_envs=self.n_envs, seed= self.seed)    
+        else:
+            env = gym.make(self.enviroment_name)
+        
+        if (self.is_image_based):
+            env = VecFrameStack(env, n_stack=self.n_stack)
+        env.reset() 
         if (self.algorithm == 'A2C'):
-            model = A2C(self.policy, env, verbose=self.verbose,tensorboard_log=self.log_path)
+#initialize default values for learing rate
+            if (self.learning_rate is None):
+                self.learning_rate = 0.0007
+            model = A2C(self.policy, env, verbose=self.verbose,learning_rate=self.learning_rate , gamma=self.gamma, tensorboard_log=self.log_path)
         elif (self.algorithm == 'PPO'):
-            model = PPO(self.policy, env, verbose=self.verbose, tensorboard_log=self.log_path)
+            if (self.learning_rate is None):
+                self.learning_rate = 0.0003
+            model = PPO(self.policy, env, verbose=self.verbose, learning_rate=self.learning_rate, gamma=self.gamma, tensorboard_log=self.log_path)
         elif (self.algorithm == 'DQN'):
-            model = DQN(self.policy, env, verbose=self.verbose, tensorboard_log=self.log_path)
+            if (self.learning_rate is None):
+                self.learning_rate = 0.0001
+            model = DQN(self.policy, env, verbose=self.verbose, learning_rate=self.learning_rate,  gamma=self.gamma,tensorboard_log=self.log_path)
         elif (self.algorithm == 'TD3'):
-            model = TD3(self.policy, env, verbose=self.verbose, tensorboard_log=self.log_path)
+            if (self.learning_rate is None):
+                self.learning_rate = 0.001
+            model = TD3(self.policy, env, verbose=self.verbose, learning_rate=self.learning_rate, gamma=self.gamma, tensorboard_log=self.log_path)
         else :
             print("This algorithm is not implemeted yet!!!")
             return
--- test.py
+++ test.py
@@ -8,16 +8,21 @@ policy = 'CnnPolicy'
 algorithm = 'A2C'
 n_envs=4
 n_stack=4
-total_timesteps=22
+total_timesteps=300000
 seed=0
 verbose=1
 fullPath = 'models/A2C_CnnPolicy_300000.zip'
+environment_type = "Atari"
+learning_rate = 0.007
+gamma = None
+is_image_based = False
 
 # evaluateModel= EvaluateModel(fullPath, enviroment_name)
 # evaluateModel.evaluateModel()
 
 
-newModel = Model(enviroment_name, algorithm, policy, n_envs, n_stack, seed, verbose,total_timesteps)
-newModel.analyzeEnv()
+newModel = Model(enviroment_name, algorithm, policy, n_envs, n_stack, seed, verbose,total_timesteps,environment_type, learning_rate, gamma, is_image_based)
+is_imageBased = newModel.analyzeEnv()
+newModel.is_image_based = is_imageBased
 newModel.trainModel()
-newModel.evaluateModel()
+# newModel.evaluateModel()
